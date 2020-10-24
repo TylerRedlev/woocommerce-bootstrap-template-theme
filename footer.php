0d18ce48a15e9867169b2af4396f7b756c85cae4
Tyler Redlev <tyler.redlev@gmail.com>
2020-10-24 07:30:35 +0300

    scripts deleted, wp_footer() function added

--- footer.php
+++ footer.php
@@ -8,8 +8,7 @@
     </footer>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <?php   wp_footer();?>
 
 </body>
 
