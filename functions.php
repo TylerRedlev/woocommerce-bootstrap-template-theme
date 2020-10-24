9c722e639213e59fb185034b6558cba46fd8d061
Tyler Redlev <tyler.redlev@gmail.com>
2020-10-24 07:32:34 +0300

    Styles and scripts added

--- functions.php
+++ functions.php
@@ -2,6 +2,15 @@
 
 function simple_bootstrap_theme_load_scripts(){
 
+    // css files
+    wp_enqueue_style("bootstrap", get_template_directory_uri(). "/assets/vendor/bootstrap/css/bootstrap.min.css", array(), "1.0", "all");
+
+    wp_enqueue_style("blog-home", get_template_directory_uri(). "/assets/css/blog-home.css", array(), "1.0", "all");
+
+    wp_enqueue_style("blog-home",  get_stylesheet_uri(), array(), "1.0", "all");
+
+    // js scripts
+    wp_enqueue_script("bootstrap", get_template_directory_uri()."/assets/vendor/bootstrap/js/bootstrap.bundle.min.js", array("jquery"), "1.0", true);
 }
 
 add_action("wp_enqueue_scripts", "simple_bootstrap_theme_load_scripts");
