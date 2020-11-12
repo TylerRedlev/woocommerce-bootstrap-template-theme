be836c3475b9b2eef9eee1d6aa9ae8adf4739673
Tyler Redlev <tyler.redlev@gmail.com>
2020-11-12 16:39:13 +0300

    Column details modified in WooCommerce theme support function

--- functions.php
+++ functions.php
@@ -36,6 +36,9 @@ function simple_bootstrap_theme_nav_config(){
         "thumbnail_image_width" => 150,
         "single_image_width" => 200,
         "product_grid" => array(
+            "default_columns" => 10,
+            "min_columns" => 2,
+            "max_columns" => 3
             
         )
     ));
