c79d112a856e3dabecb53ada71f105ccd475be51
Tyler Redlev <tyler.redlev@gmail.com>
2020-11-15 22:52:47 +0300

    General archive product page containers, product grid  and sidebar containers added

--- functions.php
+++ functions.php
@@ -70,11 +70,79 @@ remove_action( "woocommerce_sidebar", "woocommerce_get_sidebar");
 
 // add container & row class
 
-function open_container_row_div_classes{
+function open_container_row_div_classes(){
+    echo "<div class='container owt-container'><div class='row owt-row'>";
 
 }
 
-add_action("woocommerce_before_main_content", "add_container_row_div_classes", 5);
+add_action("woocommerce_before_main_content", "open_container_row_div_classes", 5);
+
+
+
+// close container & row class
+
+function close_container_row_div_classes(){
+
+    echo "</div></div>";
+}
+
+add_action("woocommerce_after_main_content", "close_container_row_div_classes", 5);
+
+
+
+
+// ** open sidebar column grid **
+// <div class="col-sm-4"> Sidebar</div>
+
+function open_sidebar_column_grid() {
+
+    echo "<div class='col-sm-4'>";
+
+}
+
+add_action("woocommerce_before_main_content", "open_sidebar_column_grid", 6);
+
+
+//Readding the sidebar after opening sidebar with our own div class
+
+add_action("woocommerce_before_main_content", "woocommerce_get_sidebar", 7);
+
+// close sidebar column grid
+
+function close_sidebar_column_grid(){
+    echo "</div>";
+}
+
+add_action("woocommerce_before_main_content", "close_sidebar_column_grid", 8);
+
+
+// Adding the products grid into the div class
+// <div class="col-sm-8"></div>
+
+function open_product_column_grid(){
+    echo "<div class='col-sm-8'>";
+}
+
+add_action("woocommerce_before_main_content", "open_product_column_grid", 9);
+
+//closing the product grid div
+
+function close_product_column_grid(){
+    echo "</div>";
+}
+
+add_action("woocommerce_before_main_content", "close_product_column_grid", 10);
+
+
+// template layout if clicked on a product item (redirecting the template to another such as a single product)
+
+function load_template_layout(){
+
+    if(is_shop()){ // True
+        
+
+    }
+}
 
 
 
