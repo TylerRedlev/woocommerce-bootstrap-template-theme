0c543a27e782e950cb789f098936dbbbd2f0bbd3
Tyler Redlev <tyler.redlev@gmail.com>
2020-11-16 16:59:34 +0300

    Added WoCommerce override templates

--- include/wc-modifications.php
+++ include/wc-modifications.php
@@ -103,4 +103,13 @@ function toggle_page_title($val){
 //Add excerpt under shop product items title
 
 add_action("woocommerce_after_shop_loop_item_title", "the_excerpt");
+
+
+// remove elements from archive-product.php
+
+remove_action( "woocommerce_before_main_content", "woocommerce_breadcrumb", 20);
+
+remove_action( "woocommerce_before_shop_loop", "woocommerce_result_count", 20 );
+
+remove_action( "woocommerce_before_shop_loop", "woocommerce_catalog_ordering", 30 );
 ?>
