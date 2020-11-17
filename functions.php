946e85b6adc681186debabca2681d601c7a8d2ab
Tyler Redlev <tyler.redlev@gmail.com>
2020-11-17 15:30:46 +0300

    style.css addition corrected

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function simple_bootstrap_theme_load_scripts(){
 
     wp_enqueue_style("blog-home", get_template_directory_uri(). "/assets/css/blog-home.css", array(), "1.0", "all");
 
-    wp_enqueue_style("blog-home",  get_stylesheet_uri(), array(), "1.0", "all");
+    wp_enqueue_style("style",  get_stylesheet_uri(), array(), "1.0", "all");
 
     // js scripts
     wp_enqueue_script("bootstrap", get_template_directory_uri()."/assets/vendor/bootstrap/js/bootstrap.bundle.min.js", array("jquery"), "1.0", true);
